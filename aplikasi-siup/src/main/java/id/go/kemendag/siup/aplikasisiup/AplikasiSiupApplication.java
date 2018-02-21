d9d6b9bbd084e1bd8fb94853703eda7b36df9253
Endy Muhardin <endy.muhardin@gmail.com>
2018-02-21 12:01:51 +0700

    starter project

---
+++ aplikasi-siup/src/main/java/id/go/kemendag/siup/aplikasisiup/AplikasiSiupApplication.java
@@ -0,0 +1,12 @@
+package id.go.kemendag.siup.aplikasisiup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikasiSiupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikasiSiupApplication.class, args);
+	}
+}
---
+++ aplikasi-siup/src/test/java/id/go/kemendag/siup/aplikasisiup/AplikasiSiupApplicationTests.java
@@ -0,0 +1,16 @@
+package id.go.kemendag.siup.aplikasisiup;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AplikasiSiupApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
