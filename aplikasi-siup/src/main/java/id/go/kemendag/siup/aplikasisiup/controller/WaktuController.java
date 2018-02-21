6935cd6d84750c8bd3b77a03c1316f27ccfca9cb
Endy Muhardin <endy.muhardin@gmail.com>
2018-02-21 14:35:34 +0700

    contoh output json

--- aplikasi-siup/src/main/java/id/go/kemendag/siup/aplikasisiup/controller/WaktuController.java
+++ aplikasi-siup/src/main/java/id/go/kemendag/siup/aplikasisiup/controller/WaktuController.java
@@ -2,9 +2,12 @@ package id.go.kemendag.siup.aplikasisiup.controller;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.LinkedHashMap;
 import java.util.Locale;
+import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -15,5 +18,18 @@ public class WaktuController {
     public String sekarang(){
         SimpleDateFormat formatterWaktu = new SimpleDateFormat("EEEE, dd MMMM yyyy", new Locale("id", "ID"));
         return formatterWaktu.format(new Date());
+    }
+    
+    @GetMapping("/appinfo")
+    @ResponseBody
+    public Map<String, String> info(@RequestParam String nama){
+        Map<String, String> data = new LinkedHashMap<>();
+        
+        data.put("application.name", "Aplikasi SIUP");
+        data.put("application.version", "1.0.0");
+        data.put("last.update", "2018-01-01");
+        data.put("author", nama);
+        
+        return data;
     }
 }
