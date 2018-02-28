c9b036ada36b09c7d9f6dd474c44b7ae1f990542
Endy Muhardin <endy.muhardin@gmail.com>
2018-02-28 10:55:53 +0700

    implement cari by id

--- aplikasi-siup/src/main/java/id/go/kemendag/siup/aplikasisiup/controller/KelurahanApiController.java
+++ aplikasi-siup/src/main/java/id/go/kemendag/siup/aplikasisiup/controller/KelurahanApiController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -18,5 +19,11 @@ public class KelurahanApiController {
     @ResponseBody
     public Page<Kelurahan> dataKelurahan(Pageable page){
         return kelurahanDao.findAll(page);
+    }
+    
+    @GetMapping("/api/kelurahan/{id}")
+    @ResponseBody
+    public Kelurahan cariKelurahanById(@PathVariable(name = "id") Kelurahan k){
+        return k;
     }
 }
