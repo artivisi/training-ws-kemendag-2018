cfd4ebee6292de7e32feeb1458e3cb96dda7fea2
Endy Muhardin <endy.muhardin@gmail.com>
2018-02-28 10:00:35 +0700

    implement rest API untuk daftar kelurahan

---
+++ aplikasi-siup/src/main/java/id/go/kemendag/siup/aplikasisiup/controller/KelurahanApiController.java
@@ -0,0 +1,22 @@
+package id.go.kemendag.siup.aplikasisiup.controller;
+
+import id.go.kemendag.siup.aplikasisiup.dao.KelurahanDao;
+import id.go.kemendag.siup.aplikasisiup.entity.Kelurahan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class KelurahanApiController {
+    @Autowired
+    private KelurahanDao kelurahanDao;
+    
+    @GetMapping("/api/kelurahan")
+    @ResponseBody
+    public Page<Kelurahan> dataKelurahan(Pageable page){
+        return kelurahanDao.findAll(page);
+    }
+}
