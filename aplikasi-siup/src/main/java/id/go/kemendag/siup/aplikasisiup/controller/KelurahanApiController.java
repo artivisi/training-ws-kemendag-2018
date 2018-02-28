8f6d4ceda8e3bd381bbddfea0f51088ce1f3290e
Endy Muhardin <endy.muhardin@gmail.com>
2018-02-28 11:50:32 +0700

    implement insert kelurahan

--- aplikasi-siup/src/main/java/id/go/kemendag/siup/aplikasisiup/controller/KelurahanApiController.java
+++ aplikasi-siup/src/main/java/id/go/kemendag/siup/aplikasisiup/controller/KelurahanApiController.java
@@ -2,13 +2,18 @@ package id.go.kemendag.siup.aplikasisiup.controller;
 
 import id.go.kemendag.siup.aplikasisiup.dao.KelurahanDao;
 import id.go.kemendag.siup.aplikasisiup.entity.Kelurahan;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class KelurahanApiController {
@@ -25,5 +30,11 @@ public class KelurahanApiController {
     @ResponseBody
     public Kelurahan cariKelurahanById(@PathVariable(name = "id") Kelurahan k){
         return k;
+    }
+    
+    @PostMapping("/api/kelurahan")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void simpan(@RequestBody @Valid Kelurahan k){
+        kelurahanDao.save(k);
     }
 }
